594cc3df80b5e14ce32e54d71b25c15f9cfef9a4
Frazier Kyle <50557956+BigNinjaChicken@users.noreply.github.com>
2024-01-22 17:35:35 -0500

    x

--- Source/SteamCompany/EnemyManagerActor.cpp
+++ Source/SteamCompany/EnemyManagerActor.cpp
@@ -1,27 +1,81 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "EnemyManagerActor.h"
+#include "GameFramework/Character.h"
+#include "EnemySpawnNodeActor.h"
+#include "CombatComponent.h"
+#include "Kismet/GameplayStatics.h"
 
 // Sets default values
 AEnemyManagerActor::AEnemyManagerActor()
 {
- 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+    // Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+    PrimaryActorTick.bCanEverTick = true;
 
+    EnemiesPerPlayer = 5; // Starting number of enemies per player
+    WaveCount = 0; // Initial wave count
 }
 
 // Called when the game starts or when spawned
 void AEnemyManagerActor::BeginPlay()
 {
-	Super::BeginPlay();
-	
+    Super::BeginPlay();
+
+    GetWorldTimerManager().SetTimer(SpawnTimerHandle, this, &AEnemyManagerActor::SpawnEnemies, SpawnInterval, true);
+
+    TArray<AActor*> Actors;
+    UGameplayStatics::GetAllActorsOfClass(GetWorld(), AEnemySpawnNodeActor::StaticClass(), Actors);
+
+    for (AActor* Actor : Actors)
+    {
+        if (AEnemySpawnNodeActor* EnemySpawnNodeActor = Cast<AEnemySpawnNodeActor>(Actor))
+        {
+            EnemySpawnNodeActors.Add(EnemySpawnNodeActor);
+        }
+    }
 }
 
 // Called every frame
 void AEnemyManagerActor::Tick(float DeltaTime)
 {
-	Super::Tick(DeltaTime);
-
+    Super::Tick(DeltaTime);
 }
 
+void AEnemyManagerActor::SpawnEnemies()
+{
+    TArray<AActor*> Players;
+    UGameplayStatics::GetAllActorsOfClass(GetWorld(), ACharacter::StaticClass(), Players);
+
+    for (AActor* Player : Players)
+    {
+        if (ACharacter* Character = Cast<ACharacter>(Player))
+        {
+            UActorComponent* CharacterComponent = Character->GetComponentByClass(UCombatComponent::StaticClass());
+            if (UCombatComponent* CombatComponent = Cast<UCombatComponent>(CharacterComponent))
+            {
+                if (CombatComponent->bIsPlayer)
+                {
+                    // Sort the spawn nodes based on distance from the player
+                    EnemySpawnNodeActors.Sort([Player](const AEnemySpawnNodeActor& A, const AEnemySpawnNodeActor& B) {
+                        return FVector::Dist(Player->GetActorLocation(), A.GetActorLocation()) < FVector::Dist(Player->GetActorLocation(), B.GetActorLocation());
+                        });
+
+                    // Spawn enemies at the closest nodes
+                    for (int32 i = 0; i < EnemiesPerPlayer; ++i)
+                    {
+                        int32 RandomIndex = FMath::RandRange(0, 4);
+                        if (EnemySpawnNodeActors[RandomIndex])
+                        {
+                            FTransform SpawnTransform = EnemySpawnNodeActors[RandomIndex]->GetActorTransform();
+                            ACharacter* SpawnedEnemyCharacter = GetWorld()->SpawnActor<ACharacter>(EnemyCharacter, SpawnTransform);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    WaveCount++;
+    if (WaveCount % 3 == 0)
+    {
+        EnemiesPerPlayer++;
+    }
+}
--- Source/SteamCompany/EnemyManagerActor.h
+++ Source/SteamCompany/EnemyManagerActor.h
@@ -1,26 +1,48 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#pragma once
-
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
+#include "TimerManager.h"
+#include "../../../../../../../Source/Runtime/Engine/Classes/GameFramework/Character.h"
+#include "EnemySpawnNodeActor.h"
 #include "EnemyManagerActor.generated.h"
+
+class AEnemySpawnNodeActor;
+class ACharacter;
 
 UCLASS()
 class STEAMCOMPANY_API AEnemyManagerActor : public AActor
 {
-	GENERATED_BODY()
-	
-public:	
-	// Sets default values for this actor's properties
-	AEnemyManagerActor();
+    GENERATED_BODY()
+    
+public:    
+    // Sets default values for this actor's properties
+    AEnemyManagerActor();
 
 protected:
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
+    // Called when the game starts or when spawned
+    virtual void BeginPlay() override;
 
-public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
+public:    
+    // Called every frame
+    virtual void Tick(float DeltaTime) override;
 
+    UPROPERTY()
+    FTimerHandle SpawnTimerHandle;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Combat")
+    int32 EnemiesPerPlayer = 3;
+
+    UPROPERTY()
+    int32 WaveCount;
+
+    UPROPERTY()
+    TArray<AEnemySpawnNodeActor*> EnemySpawnNodeActors;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Combat")
+    TSubclassOf<ACharacter> EnemyCharacter;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Combat")
+    float SpawnInterval = 15.0f;
+
+    UFUNCTION()
+    void SpawnEnemies();
 };
